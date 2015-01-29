bdf83fda64fb3abd0167b2ed8364d899178b3dfe
nikeboi89 <hoangnn@Hoangs-MacBook-Pro.local>
2015-01-29 16:39:51 +0700

    Create project source code

---
+++ CollectionViewTutorial/CollectionViewTutorial/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CollectionViewTutorial
+//
+//  Created by Hoang Nguyen Ngoc on 1/29/15.
+//  Copyright (c) 2015 HoangNN. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ CollectionViewTutorial/CollectionViewTutorial/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  CollectionViewTutorial
+//
+//  Created by Hoang Nguyen Ngoc on 1/29/15.
+//  Copyright (c) 2015 HoangNN. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
